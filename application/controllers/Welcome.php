2c84407f840ee88b00b3723a0c3e583b35dadaeb
rickykusriana <rickykusriana@gmail.com>
2017-02-04 20:43:58 +0700

    Second commit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,6 +5,7 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-            $this->load->view('welcome_message');
+		$data['haha'] = 'commit kedua';
+		$this->load->view('welcome_message', $data);
 	}
 }
