a42560dc2676abc04ccc42cc7d17c384ccfca63a
rickykusriana <rickykusriana@gmail.com>
2017-02-08 14:53:36 +0700

    Test lagi

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,7 +5,8 @@ class Dashboard extends CI_Controller {
 
 	public function index()
 	{
-			$data['apa']	= 'Test lagi';
-            $this->load->view('web', $data);
+		$data['content']	= 'asasa';
+		$data['apa']	= 'Test lagi';
+        $this->load->view('web', $data);
 	}
 }
